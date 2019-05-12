93410a6ae45fdcdd5abc91eb354cf95624b6ac5e
DrewMcArthur <DrewMcArthur1@gmail.com>
2019-05-12 11:46:37 -0400

    Updates

---
+++ precache-manifest.d274e5595ddb534110772821e196fdb2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8d54efbbb80fd5b90e9465a1b046514",
+    "url": "/mothersday/index.html"
+  },
+  {
+    "revision": "7dbecd3ee1bede4fbad9",
+    "url": "/mothersday/static/css/main.ee360868.chunk.css"
+  },
+  {
+    "revision": "71be533700c07cf46b2c",
+    "url": "/mothersday/static/js/2.0bf221b8.chunk.js"
+  },
+  {
+    "revision": "7dbecd3ee1bede4fbad9",
+    "url": "/mothersday/static/js/main.60000dc6.chunk.js"
+  },
+  {
+    "revision": "34f279782e381509906f",
+    "url": "/mothersday/static/js/runtime~main.ba18cc7c.js"
+  }
+]);
